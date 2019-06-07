5ea7eaac23dd8215adf50f2ef9206efbf7a23f19
bakalm <bakalm@umich.edu>
2019-06-06 21:21:26 -0400

    feed html now works

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -38,6 +38,6 @@ public class MessageFeedServlet extends HttpServlet{
   Gson gson = new Gson();
   String json = gson.toJson(messages);
   
-  response.getOutputStream().println(json);
+  response.getWriter().println(json);
  }
 }
